3df4ff52abff138667aefada782f38994d144e62
Rodrigo Ferreira Rodrigues <88565286+guigomaster01@users.noreply.github.com>
2025-02-02 07:49:00 -0300

    edit deploy.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
